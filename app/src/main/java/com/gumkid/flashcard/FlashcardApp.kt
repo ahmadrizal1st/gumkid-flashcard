8835da976fca713d1045bcaf8fda9fa5265fe76d
Ahmad Rizal <ahmaddrizalul@gmail.com>
2025-12-14 22:44:26 +0700

    firebase initialization

--- app/src/main/java/com/gumkid/flashcard/FlashcardApp.kt
+++ app/src/main/java/com/gumkid/flashcard/FlashcardApp.kt
@@ -2,12 +2,13 @@ package com.gumkid.flashcard
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import com.google.firebase.FirebaseApp
 
 @HiltAndroidApp
 class FlashcardApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        // Firebase initialization is handled automatically by Firebase SDK
-    }
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this)    }
 }
